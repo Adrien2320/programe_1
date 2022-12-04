a0fb6a1faa4e120ee69b00210f3bc9ed160cac8c
Adrien <Mertensadrien23@hotmail.be>
2022-12-04 20:52:16 +0100

    version 6
    ajout des dernière fonctionnalité du menu principale.

--- api.py
+++ api.py
@@ -1,24 +1,40 @@
 import random
 
 # variable global
-dict_login : dict = {("A","1"): 22 }
-dict_chest : dict = {0:56}
+dict_login: dict = {("Adrien", "1234"): 600}
+dict_chest: dict = {600:"Biére"}
 
+def get_chest_key(name_and_password_of_user: tuple)-> int:
+    """cherche la clé de coffre lier a le typle name_and_password_of_user dans un dictionnaire"""
+    # type and assign
+    key: int = dict_login[name_and_password_of_user]
+    return key
 
-def show_login_menu() -> None :
-    pass
-    #show_chest_menu(key)
-#todo créer la menu pour se connecter a son compte et le mettre dans la function show_login_menu
+def show_login_menu() -> None:
+    """affiche le menu pour se connecter et se connect sur le coffre de l'utilisateur."""
+    # type and assign
+    answer: tuple
+    key: int
+    # initiate the login menu
+    print("connection a votre coffre".center(100, "_"),
+          "\n")
+    #
+    answer = resquest_name_and_passwords()
+    if answer in dict_login:
+        key = get_chest_key(answer)
+        show_chest_menu(key)
+    else:
+        print("mot de passe ou nom utilisateur incorrect, veuillez ressayez")
+        show_main_menu()
 
-
-def resquest_name_and_passwords()-> tuple:
+def resquest_name_and_passwords() -> tuple:
     """ Demande à l'utilisateur, un nom et un mot de passe. Et le retourne sour un tuple (name,password)"""
-    name : str =input("nom utilisateur:")
-    password : str = input("Mots de passe:")
-    return name , password
+    name: str = input("nom utilisateur:")
+    password: str = input("Mot de passe:")
+    return name, password
 
 
-def check_if_exist(name_and_password_user,dict_search:dict)-> bool:
+def check_if_exist(name_and_password_user, dict_search: dict) -> bool:
     """Verify si le nom et le mot de passe exist déjà """
     if name_and_password_user in dict_search:
         return True
@@ -26,30 +42,31 @@ def check_if_exist(name_and_password_user,dict_search:dict)-> bool:
         return False
 
 
-def create_key()->int:
+def create_key() -> int:
     """Crée une key aléatoire et unique"""
-    key : int = 0
-    while check_if_exist(key,dict_chest):
+    key: int = 0
+    while check_if_exist(key, dict_chest):
         key = random.randint(1, 1000)
     return key
 
 
-def show_chest_menu(key:int)->None:
+def show_chest_menu(key: int) -> None:
     pass
-#todo créer le menu pour le coffre dans la function show_chest_menu
 
-def show_create_new_user_menu()-> None:
+# todo créer le menu pour le coffre dans la function show_chest_menu
+
+def show_create_new_user_menu() -> None:
     """ Affiche le menu creation utilisateur et le crée """
-    #type and assign
+    # type and assign
     global dict_login
-    name_pass : tuple
+    name_pass: tuple
     #
     print("création nouvelle utilisateur".center(100, "_"),
           "\n "
           )
 
     name_pass = resquest_name_and_passwords()
-    if check_if_exist(name_pass,dict_login) :
+    if check_if_exist(name_pass, dict_login):
         print("\n"
               "nom d'utilisateur existe déjà, Veuillez en trouver un autre!")
         show_create_new_user_menu()
@@ -58,9 +75,20 @@ def show_create_new_user_menu()-> None:
         dict_login = {name_pass: key}
         show_chest_menu(key)
 
-def show_remove_user_menu()-> None:
-    pass
 
+def show_remove_user_menu() -> None:
+    """affiche le menu pour supprimer un utilisateur et le supprime"""
+    # initiate the remove user menu
+    print("suppression de votre compte".center(100,"_"),
+          "\n")
+    #type and assign
+    name_password : tuple = resquest_name_and_passwords()
+    key_chest : int = get_chest_key(name_password)
+    del dict_login[name_password]
+    del dict_chest[key_chest]
+    print("votre compte, a bien été supprimer"
+          "\n au revoir")
+    show_main_menu()
 
 def show_main_menu() -> None:
     """affiche le principale de l'application"""
