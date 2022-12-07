0a2deb93293fdd3337c6e4d625a1908615e1ae1e
Adrien <Mertensadrien23@hotmail.be>
2022-12-07 11:33:57 +0100

    version 7
    add of missing comments

--- api.py
+++ api.py
@@ -2,49 +2,63 @@ import random
 
 # variable global
 dict_login: dict = {("Adrien", "1234"): 600}
-dict_chest: dict = {600:"Biére"}
+dict_chest: dict = {600: "Biére"}
 
-def get_chest_key(name_and_password_of_user: tuple)-> int:
-    """cherche la clé de coffre lier a le typle name_and_password_of_user dans un dictionnaire"""
+
+def get_chest_key(name_and_password_of_user: tuple) -> int:
+    """cherche la clé de coffre lier au tuple name_and_password_of_user dans un dictionnaire"""
     # type and assign
     key: int = dict_login[name_and_password_of_user]
+    # return of the chest key
     return key
 
+
 def show_login_menu() -> None:
-    """affiche le menu pour se connecter et se connect sur le coffre de l'utilisateur."""
+    """affiche le menu pour se connecter ."""
     # type and assign
     answer: tuple
     key: int
     # initiate the login menu
     print("connection a votre coffre".center(100, "_"),
           "\n")
-    #
+    # assign answer with the select of the user
     answer = resquest_name_and_passwords()
+    # check if the user's answer is found in the dict_login
     if answer in dict_login:
+        #  get the chest key link with the user answer and start the chest menu
         key = get_chest_key(answer)
         show_chest_menu(key)
     else:
+        #  show that the user answer is incorrect and return in the main menu
         print("mot de passe ou nom utilisateur incorrect, veuillez ressayez")
         show_main_menu()
 
+
 def resquest_name_and_passwords() -> tuple:
-    """ Demande à l'utilisateur, un nom et un mot de passe. Et le retourne sour un tuple (name,password)"""
+    """ Demande à l'utilisateur, un nom et un mot de passe. Et le retourne sous un tuple (name,password)"""
+    # type and assign
     name: str = input("nom utilisateur:")
     password: str = input("Mot de passe:")
+    # return the user's answers into a tuple
     return name, password
 
 
 def check_if_exist(name_and_password_user, dict_search: dict) -> bool:
     """Verify si le nom et le mot de passe exist déjà """
+    # check if the user's answer exist in the dict_search
     if name_and_password_user in dict_search:
+        # the user's answer not exist in dict_search and return true
         return True
     else:
+        # the user's answer exist in dict_search and return False
         return False
 
 
 def create_key() -> int:
-    """Crée une key aléatoire et unique"""
+    """Créer une key aléatoire et unique"""
+    # type and assign
     key: int = 0
+    # check if the key exist in dict_chest and if yes new create
     while check_if_exist(key, dict_chest):
         key = random.randint(1, 1000)
     return key
@@ -53,24 +67,26 @@ def create_key() -> int:
 def show_chest_menu(key: int) -> None:
     pass
 
-# todo créer le menu pour le coffre dans la function show_chest_menu
 
 def show_create_new_user_menu() -> None:
-    """ Affiche le menu creation utilisateur et le crée """
+    """ Affiche le menu creation utilisateur et le créer """
     # type and assign
     global dict_login
     name_pass: tuple
-    #
+    # initiate the create new user menu
     print("création nouvelle utilisateur".center(100, "_"),
           "\n "
           )
-
-    name_pass = resquest_name_and_passwords()
-    if check_if_exist(name_pass, dict_login):
+    # assign name_password with the user's answer
+    name_password = resquest_name_and_passwords()
+    # check if the user's answer exist in dict_login
+    if check_if_exist(name_password, dict_login):
+        #  show that the name and password exist in dict_login and restart the create new user menu
         print("\n"
               "nom d'utilisateur existe déjà, Veuillez en trouver un autre!")
         show_create_new_user_menu()
     else:
+        # type and assign key, create the new user and start the chest menu
         key = create_key()
         dict_login = {name_pass: key}
         show_chest_menu(key)
@@ -79,16 +95,19 @@ def show_create_new_user_menu() -> None:
 def show_remove_user_menu() -> None:
     """affiche le menu pour supprimer un utilisateur et le supprime"""
     # initiate the remove user menu
-    print("suppression de votre compte".center(100,"_"),
+    print("suppression utilisateur".center(100, "_"),
           "\n")
-    #type and assign
-    name_password : tuple = resquest_name_and_passwords()
-    key_chest : int = get_chest_key(name_password)
+    # type and assign
+    name_password: tuple = resquest_name_and_passwords()
+    key_chest: int = get_chest_key(name_password)
+    # delete user in the dict_login
     del dict_login[name_password]
     del dict_chest[key_chest]
+    # show that the account has been successfully delete and start the main menu
     print("votre compte, a bien été supprimer"
           "\n au revoir")
     show_main_menu()
+
 
 def show_main_menu() -> None:
     """affiche le principale de l'application"""
